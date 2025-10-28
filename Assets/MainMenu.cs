262d3d80c7e59b0749594c69e2de83ce3930fafb
Kelzin <jade.raquels2020@gmail.com>
2025-10-28 09:24:29 -0300

    Feat: Atualizei o Menu e coloquei o Game Fixing

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -11,7 +11,7 @@ public class MainMenu : MonoBehaviour
     
     public void StartGame()
     {
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("Game Fixing");
         
     }
 }
